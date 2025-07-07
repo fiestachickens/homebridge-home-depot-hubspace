dca13b0268bb05f86856c9642ebefb65d5758f12
Cyrus Keck <fiestachickens@gmail.com>
2025-07-07 07:15:42 -0400

    Attempting another name fix

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { API } from 'homebridge';
 import { HubspacePlatform } from './platform';
 
-import { PLATFORM_NAME } from './settings';
+import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
 
 /**
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-    api.registerPlatform(PLATFORM_NAME, HubspacePlatform);
+    api.registerPlatform(PLATFORM_NAME, PLUGIN_NAME, HubspacePlatform);
 };
