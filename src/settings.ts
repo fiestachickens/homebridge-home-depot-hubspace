f28f31d651cd7dceb1ff2bebbba188be9d648747
Cyrus Keck <fiestachickens@gmail.com>
2025-07-07 07:24:52 -0400

    Reverting

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'HomeDepotHubspace';
+export const PLATFORM_NAME = 'Hubspace';
 
 /**
  * This must match the name of your plugin as defined the package.json
